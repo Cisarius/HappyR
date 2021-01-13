816ec401c3dea4b1bea8bf46bc88f245a9dfae0b
Guillaume <guillaume.balestreri@epitech.eu>
2021-01-13 13:56:51 +0100

    Exo3

--- App.js
+++ App.js
@@ -1,24 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, View, Text, TouchableOpacity, Alert} from 'react-native';
+import { SectionList, Text, TouchableOpacity, Alert} from 'react-native';
 
 
 export default function App() {
 
+const ListByCountry = (Input = []) => {
+ const data = [...Input];
+
+ const filtereddata = data.reduce((Stack, item) => {
+   const group = item.country;
+  if (Stack[group]) {
+    Stack[group].data.push(item);
+  } else {
+    Stack[group] = {
+      title: group,
+      data: [item],
+    };
+  }
+  return Stack;
+ })
+ console.log(filtereddata)
+ const Filtered = Object.keys(filtereddata).map((key) => {
+   return filtereddata[key];
+ })
+ return filtereddata;
+}
+
   return (
-    <>
-    <FlatList
-    data={cities}
-    renderItem={({item})=>(
+    <SectionList
+    sections={ListByCountry(cities)}
+    renderSectionHeader={({section})=>{
+      return (
+        <View>
+          <Text>{section.title}</Text>
+        </View>
+      );
+    }}
+      renderItem={({item}) => (
       <TouchableOpacity onPress={() => Alert.alert('Pressed')}
        style={{justifyContent:'center',marginBottom:10}}>
       <Text style={{backgroundColor: 'palegreen',color:'white',padding:10}}>
-      {item.name}  {item.country}
+      {item.name}
       </Text>
       </TouchableOpacity>
     )}
   />
-  </>
   );
 }
 
